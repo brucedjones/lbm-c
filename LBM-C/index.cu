3d2eb791c961f2552d4cbef41db97d10e819a0d2
Bruce Jones <bruce.david.jones@gmail.com>
2011-10-22 11:57:32 +0100

    Included GPL v2 preamble

--- LBM-C/index.cu
+++ LBM-C/index.cu
@@ -2,19 +2,38 @@
 #define KERNEL
 ////////////////////////////////////////////////////////////////////////////////
 //
-// This is a 15 velocity set method:
-// Distribution functions are stored as "f" arrays
-// Think of these as the number of particles moving in these directions:
+// LBM-C
+// A lattice Boltzmann fluid flow solver written using CUDA
 //
-//      f6  f2   f5
+// Copyright (C) 2011  Bruce Jones
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// D2Q9 Lattice configuration:
+//
+//       6   2   5
 //        \  |  /
 //         \ | /
 //          \|/
-//      f3---|--- f1
+//       3---0---1
 //          /|\
-//         / | \       and f0 for the rest (zero) velocity
+//         / | \       
 //        /  |  \
-//      f7  f4   f8
+//       7   4   8
 //
 ///////////////////////////////////////////////////////////////////////////////
 
