086b4a74f5045d1562bce257d61f2415b2603c07
Bruce Jones <bruce.david.jones@gmail.com>
2016-08-21 16:37:06 -0400

    Update index.cu

--- LBM-C/index.cu
+++ LBM-C/index.cu
@@ -1,41 +1,5 @@
 #ifndef KERNEL
 #define KERNEL
-////////////////////////////////////////////////////////////////////////////////
-//
-// LBM-C
-// A lattice Boltzmann fluid flow solver written using CUDA
-//
-// Copyright (C) 2011  Bruce Jones
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-//
-////////////////////////////////////////////////////////////////////////////////
-//
-// D2Q9 Lattice configuration:
-//
-//       6   2   5
-//        \  |  /
-//         \ | /
-//          \|/
-//       3---0---1
-//          /|\
-//         / | \
-//        /  |  \
-//       7   4   8
-//
-///////////////////////////////////////////////////////////////////////////////
 
 #ifdef _WIN64
 	#pragma comment(lib, "cgns/x64/lib/cgns.lib")
